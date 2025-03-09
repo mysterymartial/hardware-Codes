eb92681030464f62fd2d86630a557219e817333b
mysterymartial <bolasax16@gmail.com>
2025-03-09 04:23:17 +0100

    update

---
+++ sketch_mar9c/sketch_mar9c.ino
@@ -0,0 +1,17 @@
+const int ledPin = 12;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+
+}
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(2000);
+
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+
+}
